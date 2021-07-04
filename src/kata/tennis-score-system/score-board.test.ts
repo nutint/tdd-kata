a0bf8c594c0082a86e960ecd09ab6c1cfbe8515c
Nat <nat@Nats-Mac-mini.local>
2021-07-04 20:40:58 +0700

    Calculate first player score when first player make point

--- src/kata/tennis-score-system/score-board.test.ts
+++ src/kata/tennis-score-system/score-board.test.ts
@@ -12,7 +12,7 @@ describe('ScoreBoard', () => {
     it('should begin at not-started state', () => {
       const actual = ScoreBoard()
 
-      expect(actual.boardStatus).toEqual('notStarted')
+      expect(actual.boardStatus).toEqual(BoardStatus.NotStarted)
     })
 
     it('should start at 15 0 when specify first player score', () => {
@@ -33,6 +33,30 @@ describe('ScoreBoard', () => {
       const actual = ScoreBoard(BoardStatus.Deuce)
 
       expect(actual.boardStatus).toEqual(BoardStatus.Deuce)
+    })
+  })
+
+  describe('xPlayerMakeThePoint', () => {
+    it('started at 0-0, when first player make the point should become 15-0', () => {
+      const actual = ScoreBoard().firstPlayerMakeThePoint()
+
+      expect(actual.firstPlayerScore).toEqual(PlayerScore.Fifteen)
+      expect(actual.secondPlayerScore).toEqual(PlayerScore.Love)
+    })
+
+    it('started at 0-0, when first player make the point should have status started', () => {
+      const actual = ScoreBoard().firstPlayerMakeThePoint()
+
+      expect(actual.boardStatus).toEqual(BoardStatus.Started)
+    })
+
+    it('started at 0-0, when first player make the point 2 times should become 30-0', () => {
+      const actual = ScoreBoard()
+        .firstPlayerMakeThePoint()
+        .firstPlayerMakeThePoint()
+
+      expect(actual.firstPlayerScore).toEqual(PlayerScore.Thirty)
+      expect(actual.secondPlayerScore).toEqual(PlayerScore.Love)
     })
   })
 })
