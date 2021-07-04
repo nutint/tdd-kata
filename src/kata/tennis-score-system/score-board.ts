e5a98e7f2f30d58ceabcf10c7041e5a80fbb9148
Nat <nat@Nats-Mac-mini.local>
2021-07-04 19:59:07 +0700

    Add constructor for score board

---
+++ src/kata/tennis-score-system/score-board.ts
@@ -0,0 +1,4 @@
+export const ScoreBoard = (firstPlayerScore?: string, secondPlayerScore?: string) => ({
+  firstPlayerScore: firstPlayerScore || '0',
+  secondPlayerScore: secondPlayerScore || '0'
+})
