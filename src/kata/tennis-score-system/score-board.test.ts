5ae50d46b9b6971fa39f6a2e52c15b2e92272633
Nat <nat@Nats-Mac-mini.local>
2021-07-04 20:11:11 +0700

    Add board status

--- src/kata/tennis-score-system/score-board.test.ts
+++ src/kata/tennis-score-system/score-board.test.ts
@@ -9,18 +9,30 @@ describe('ScoreBoard', () => {
       expect(actual.secondPlayerScore).toEqual('0')
     })
 
+    it('should begin at not-started state', () => {
+      const actual = ScoreBoard()
+
+      expect(actual.boardStatus).toEqual('notStarted')
+    })
+
     it('should start at 15 0 when specify first player score', () => {
-      const actual = ScoreBoard('15')
+      const actual = ScoreBoard('started', '15')
 
       expect(actual.firstPlayerScore).toEqual('15')
       expect(actual.secondPlayerScore).toEqual('0')
     })
 
     it('should start at 15 15 when specify first player score', () => {
-      const actual = ScoreBoard('15', '15')
+      const actual = ScoreBoard('started', '15', '15')
 
       expect(actual.firstPlayerScore).toEqual('15')
       expect(actual.secondPlayerScore).toEqual('15')
+    })
+
+    it('should be able to start at deuce state', () => {
+      const actual = ScoreBoard('deuce')
+
+      expect(actual.boardStatus).toEqual('deuce')
     })
   })
 })
