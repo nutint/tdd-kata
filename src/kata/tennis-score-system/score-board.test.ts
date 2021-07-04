4c55f8eb886d92ad9d736c92b6c9d29b502b4b42
Nat <nat@Nats-Mac-mini.local>
2021-07-04 20:16:30 +0700

    Get rid of migic values

--- src/kata/tennis-score-system/score-board.test.ts
+++ src/kata/tennis-score-system/score-board.test.ts
@@ -1,12 +1,12 @@
-import {ScoreBoard} from './score-board'
+import {BoardStatus, PlayerScore, ScoreBoard} from './score-board'
 
 describe('ScoreBoard', () => {
   describe('constructor', () => {
     it('should start at 0 0 when the score board is created', () => {
       const actual = ScoreBoard()
 
-      expect(actual.firstPlayerScore).toEqual('0')
-      expect(actual.secondPlayerScore).toEqual('0')
+      expect(actual.firstPlayerScore).toEqual(PlayerScore.Love)
+      expect(actual.secondPlayerScore).toEqual(PlayerScore.Love)
     })
 
     it('should begin at not-started state', () => {
@@ -16,23 +16,23 @@ describe('ScoreBoard', () => {
     })
 
     it('should start at 15 0 when specify first player score', () => {
-      const actual = ScoreBoard('started', '15')
+      const actual = ScoreBoard(BoardStatus.Started, PlayerScore.Fifteen)
 
-      expect(actual.firstPlayerScore).toEqual('15')
-      expect(actual.secondPlayerScore).toEqual('0')
+      expect(actual.firstPlayerScore).toEqual(PlayerScore.Fifteen)
+      expect(actual.secondPlayerScore).toEqual(PlayerScore.Love)
     })
 
     it('should start at 15 15 when specify first player score', () => {
-      const actual = ScoreBoard('started', '15', '15')
+      const actual = ScoreBoard(BoardStatus.Started, PlayerScore.Fifteen, PlayerScore.Fifteen)
 
-      expect(actual.firstPlayerScore).toEqual('15')
-      expect(actual.secondPlayerScore).toEqual('15')
+      expect(actual.firstPlayerScore).toEqual(PlayerScore.Fifteen)
+      expect(actual.secondPlayerScore).toEqual(PlayerScore.Fifteen)
     })
 
     it('should be able to start at deuce state', () => {
-      const actual = ScoreBoard('deuce')
+      const actual = ScoreBoard(BoardStatus.Deuce)
 
-      expect(actual.boardStatus).toEqual('deuce')
+      expect(actual.boardStatus).toEqual(BoardStatus.Deuce)
     })
   })
 })
