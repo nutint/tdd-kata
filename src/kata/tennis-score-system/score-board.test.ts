585945aeab5a05cce1e9227319311a0603d93f19
Nat <nat@Nats-Mac-mini.local>
2021-07-04 19:59:07 +0700

    Add constructor for score board

--- src/kata/tennis-score-system/score-board.test.ts
+++ src/kata/tennis-score-system/score-board.test.ts
@@ -1,7 +1,26 @@
+import {ScoreBoard} from './score-board'
+
 describe('ScoreBoard', () => {
   describe('constructor', () => {
-    it('should work', () => {
-      expect(true).toEqual(true)
+    it('should start at 0 0 when the score board is created', () => {
+      const actual = ScoreBoard()
+
+      expect(actual.firstPlayerScore).toEqual('0')
+      expect(actual.secondPlayerScore).toEqual('0')
+    })
+
+    it('should start at 15 0 when specify first player score', () => {
+      const actual = ScoreBoard('15')
+
+      expect(actual.firstPlayerScore).toEqual('15')
+      expect(actual.secondPlayerScore).toEqual('0')
+    })
+
+    it('should start at 15 15 when specify first player score', () => {
+      const actual = ScoreBoard('15', '15')
+
+      expect(actual.firstPlayerScore).toEqual('15')
+      expect(actual.secondPlayerScore).toEqual('15')
     })
   })
 })
