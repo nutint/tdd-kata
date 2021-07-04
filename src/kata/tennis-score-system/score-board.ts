481d64b31793df94d2e861c0a8e4571c5b60a34d
Nat <nat@Nats-Mac-mini.local>
2021-07-04 20:40:58 +0700

    Calculate first player score when first player make point

--- src/kata/tennis-score-system/score-board.ts
+++ src/kata/tennis-score-system/score-board.ts
@@ -2,18 +2,29 @@ export const ScoreBoard = (
   boardStatus?: BoardStatus,
   firstPlayerScore?: PlayerScore,
   secondPlayerScore?: PlayerScore
-) => ({
-  boardStatus: boardStatus || 'notStarted',
-  firstPlayerScore: firstPlayerScore || '0',
-  secondPlayerScore: secondPlayerScore || '0'
-})
+) => {
+  const initialFirstPlayerScore = firstPlayerScore || PlayerScore.Love;
+  const initialSecondPlayerScore = secondPlayerScore || PlayerScore.Love;
+  return ({
+    boardStatus: boardStatus || 'notStarted',
+    firstPlayerScore: initialFirstPlayerScore,
+    secondPlayerScore: initialSecondPlayerScore,
+    firstPlayerMakeThePoint: () => ScoreBoard(
+      BoardStatus.Started,
+      initialFirstPlayerScore === PlayerScore.Love ? PlayerScore.Fifteen : PlayerScore.Thirty,
+      initialSecondPlayerScore
+    )
+  });
+}
 
 export enum PlayerScore {
   Love = '0',
-  Fifteen = '15'
+  Fifteen = '15',
+  Thirty = '30',
 }
 
 export enum BoardStatus {
+  NotStarted = 'notStarted',
   Started = 'started',
   Deuce = 'deuce'
 }
